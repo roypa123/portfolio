517352ef7eb2268148a55760b35350dc67609d7b
Roy P A <roy@nintrivalabs.com>
2025-08-06 12:12:48 +0530

    fix: some changes

--- apps/roypaportfolio/vite.config.ts
+++ apps/roypaportfolio/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
   root: __dirname,
+  base: '/portfolio/',
   cacheDir: '../../node_modules/.vite/apps/roypaportfolio',
   server: {
     port: 4200,
